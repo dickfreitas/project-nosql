2391bd927bc72d6454015d5d96dc97f24def85c3
Dickson Freitas <dickfreitas12@gmaill.com>
2024-04-18 15:49:30 -0300

    Primeiro commit

---
+++ src/main/java/com/projectnosql/projectnosql/DicksonFreitasApplication.java
@@ -0,0 +1,13 @@
+package com.projectnosql.projectnosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicksonFreitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicksonFreitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectnosql/projectnosql/DicksonFreitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectnosql.projectnosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicksonFreitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
