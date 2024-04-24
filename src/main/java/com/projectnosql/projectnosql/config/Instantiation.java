03a41347445b1acc1953644981b9634ab10a3860
Dickson Freitas <dickfreitas12@gmaill.com>
2024-04-24 15:01:24 -0300

    AuthorDTO

--- src/main/java/com/projectnosql/projectnosql/config/Instantiation.java
+++ src/main/java/com/projectnosql/projectnosql/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.projectnosql.projectnosql.config;
 
 import com.projectnosql.projectnosql.domain.Post;
 import com.projectnosql.projectnosql.domain.User;
+import com.projectnosql.projectnosql.dto.AuthorDTO;
 import com.projectnosql.projectnosql.repository.PostRepository;
 import com.projectnosql.projectnosql.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,11 +29,13 @@ public class Instantiation implements CommandLineRunner {
         User u2 = new User(null , "Pedro Brown" , "pedro@gmail.com");
         User u3 = new User(null , "Bob grey" , "bob@gmail.com");
 
-        Post p1 = new Post(null ,sdf.parse("21/03/2024"), "Vou viajar amanha" , "Irei viajar para fernando de noronha" , u1 );
-        Post p2 = new Post(null ,sdf.parse("21/03/2024"), "Pedalada matinal" , "200km frio frio" , u1 );
-        Post p3 = new Post(null ,sdf.parse("22/03/2024"), "Virar triatleta" , "A loucura total" , u2 );
-
         userRepository.saveAll(Arrays.asList(u1,u2,u3));
+
+        Post p1 = new Post(null ,sdf.parse("21/03/2024"), "Vou viajar amanha" , "Irei viajar para fernando de noronha" , new AuthorDTO(u1) );
+        Post p2 = new Post(null ,sdf.parse("21/03/2024"), "Pedalada matinal" , "200km frio frio" , new AuthorDTO(u1) );
+        Post p3 = new Post(null ,sdf.parse("22/03/2024"), "Virar triatleta" , "A loucura total" , new AuthorDTO(u2) );
+
+
         postRepository.saveAll(Arrays.asList(p1,p2,p3));
 
 
